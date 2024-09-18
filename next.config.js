e8231c69bf6c8f2d6ae672883533fc39a3e73481
Phan Thanh Hải <hai.pt@oryza.vn>
2024-09-18 13:46:17 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    console.log("Backend domain: ", process.env.NEXT_PUBLIC_BACKEND_DOMAIN);
-
-    return [
-      {
-        source: "/service/:path*",
-        destination: process.env.NEXT_PUBLIC_BACKEND_DOMAIN + "/:path*",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
